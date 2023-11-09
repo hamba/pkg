ab886917b57b5fa06b802d3578ae26a4e7e62100
Nicholas Wiersma <nick@wiersma.co.za>
2023-11-09 09:19:49 +0200

    feat: add healthz and server (#101)

--- http/server_test.go
+++ http/server_test.go
@@ -6,10 +6,14 @@ import (
 	"io"
 	"net"
 	"net/http"
+	"sync"
 	"testing"
 	"time"
 
+	"github.com/hamba/logger/v2"
 	httpx "github.com/hamba/pkg/v2/http"
+	"github.com/hamba/pkg/v2/http/healthz"
+	"github.com/hamba/statter/v2"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"golang.org/x/net/http2"
@@ -79,4 +83,108 @@ func TestServer_WithH2C(t *testing.T) {
 
 	assert.Equal(t, res.StatusCode, http.StatusOK)
 	assert.True(t, handlerCalled)
+}
+
+func TestHealthServer(t *testing.T) {
+	stats := statter.New(statter.DiscardReporter, time.Minute)
+	log := logger.New(io.Discard, logger.LogfmtFormat(), logger.Error)
+
+	check := healthz.NamedCheck("test", func(*http.Request) error {
+		return nil
+	})
+
+	srv := httpx.NewHealthServer(context.Background(), httpx.HealthServerConfig{
+		Addr:    ":16543",
+		Handler: http.HandlerFunc(func(http.ResponseWriter, *http.Request) {}),
+		Stats:   stats,
+		Log:     log,
+	})
+	err := srv.AddHealthzChecks(check)
+	require.NoError(t, err)
+	srv.Serve(func(err error) {
+		require.NoError(t, err)
+	})
+	t.Cleanup(func() {
+		_ = srv.Close()
+	})
+
+	statusCode, body := requireDoRequest(t, "http://localhost:16543/readyz?verbose=1")
+	assert.Equal(t, statusCode, http.StatusOK)
+	want := `+ test ok
++ shutdown ok
+readyz check passed`
+	assert.Equal(t, want, body)
+
+	statusCode, body = requireDoRequest(t, "http://localhost:16543/livez?verbose=1")
+	assert.Equal(t, statusCode, http.StatusOK)
+	want = `+ test ok
+livez check passed`
+	assert.Equal(t, want, body)
+}
+
+func TestHealthServer_ShutdownCausesReadyzCheckToFail(t *testing.T) {
+	stats := statter.New(statter.DiscardReporter, time.Minute)
+	log := logger.New(io.Discard, logger.LogfmtFormat(), logger.Error)
+
+	calledCh := make(chan struct{})
+	check := healthz.NamedCheck("test", func(*http.Request) error {
+		close(calledCh)
+		time.Sleep(time.Millisecond)
+		return nil
+	})
+
+	srv := httpx.NewHealthServer(context.Background(), httpx.HealthServerConfig{
+		Addr:    ":16543",
+		Handler: http.HandlerFunc(func(http.ResponseWriter, *http.Request) {}),
+		Stats:   stats,
+		Log:     log,
+	})
+	err := srv.AddHealthzChecks(check)
+	require.NoError(t, err)
+	srv.Serve(func(err error) {
+		require.NoError(t, err)
+	})
+	t.Cleanup(func() {
+		_ = srv.Close()
+	})
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		<-calledCh
+
+		err = srv.Shutdown(time.Second)
+		require.NoError(t, err)
+	}()
+
+	statusCode, body := requireDoRequest(t, "http://localhost:16543/readyz?verbose=1")
+	assert.Equal(t, statusCode, http.StatusInternalServerError)
+	want := `+ test ok
+- shutdown failed
+readyz check failed
+`
+	assert.Equal(t, want, body)
+
+	wg.Wait()
+}
+
+func requireDoRequest(t *testing.T, path string) (int, string) {
+	t.Helper()
+
+	req, err := http.NewRequest(http.MethodGet, path, nil)
+	require.NoError(t, err)
+
+	resp, err := http.DefaultClient.Do(req)
+	require.NoError(t, err)
+	defer func() {
+		_, _ = io.Copy(io.Discard, resp.Body)
+		_ = resp.Body.Close()
+	}()
+
+	b, err := io.ReadAll(resp.Body)
+	require.NoError(t, err)
+
+	return resp.StatusCode, string(b)
 }
