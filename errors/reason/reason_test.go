c80e0fa8d344adb48dcb57ed3742f06c777e80ac
antiphp <antiphp@users.noreply.github.com>
2024-10-02 12:55:42 +0200

    feat: add errors/reason (#135)

---
+++ errors/reason/reason_test.go
@@ -0,0 +1,25 @@
+package reason_test
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+
+	"github.com/hamba/pkg/v2/errors/reason"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestExtract(t *testing.T) {
+	var errs error
+	errs = errors.Join(errs, errors.New("test1"))
+	errs = errors.Join(errs, reason.Error{Msg: "First Error"})
+	errs = errors.Join(errs, fmt.Errorf("some error: %w", reason.Errorf("Second %s", "Error")))
+	errs = errors.Join(errs, errors.New("test2"))
+
+	reasons, errs := reason.Extract(errs)
+
+	require.NotEmpty(t, errs)
+	assert.Equal(t, "test1\ntest2", errs.Error())
+	assert.Equal(t, []string{"First Error", "Second Error"}, reasons)
+}
